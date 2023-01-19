cac9d3285d9b1b9eef754dd78adf5bede0604b84
Jacob Amar <jacob.aa.amar@gmail.com>
2023-01-18 19:32:23 -0500

    specifiying node version for netlify

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,19 +8,19 @@
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
+// import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+  // const data = useStaticQuery(graphql`
+  //   query SiteTitleQuery {
+  //     site {
+  //       siteMetadata {
+  //         title
+  //       }
+  //     }
+  //   }
+  // `)
 
   return (
     <>
